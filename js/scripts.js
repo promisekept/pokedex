c1c251953f02e1ec35660211faa84971fbd145d6
H A <promisekept836@outlook.com>
2021-06-06 23:56:55 -0400

    created an array of pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
-alert("Hello world");
-let favoriteFood = "Scrambled eggs";
-document.write(favoriteFood);
+let pokemonList = [];
+pokemonList.push(
+  { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
+  { name: "Ivysaur", height: 1, types: ["grass", "poison"] },
+  { name: "Pidgey", height: 0.3, types: ["flying", "normal"] },
+  { name: "Nidoking", height: 1.4, types: ["ground", "poison"] }
+);
+console.log(pokemonList);
