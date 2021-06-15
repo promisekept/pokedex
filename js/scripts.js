7087e506e62fcfbe947ca0590298900eec5e2149
H A <promisekept836@outlook.com>
2021-06-15 00:26:28 -0400

    added the IIFE and modified the .forEach

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,23 @@
-let pokemonList = [];
-pokemonList.push(
-  { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
-  { name: "Ivysaur", height: 1, types: ["grass", "poison"] },
-  { name: "Pidgey", height: 0.3, types: ["flying", "normal"] },
-  { name: "Nidoking", height: 1.4, types: ["ground", "poison"] }
-);
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
+    { name: "Ivysaur", height: 1, types: ["grass", "poison"] },
+    { name: "Pidgey", height: 0.3, types: ["flying", "normal"] },
+    { name: "Nidoking", height: 1.4, types: ["ground", "poison"] },
+  ];
+  function add(name, height, type) {
+    pokemonList.push(name, height, type);
+  }
+  function getAll() {
+    return pokemonList;
+  }
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
 
-pokemonList.forEach(function (pokemon) {
+pokemonRepository.getAll().forEach(function (pokemon) {
   document.write(`${pokemon.name} (height: ${pokemon.height}) `);
   if (pokemon.height >= 1) {
     document.write(`Wow, that's big!`);
