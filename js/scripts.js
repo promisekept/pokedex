6ea4aedef0821847cdcc0560e0619912bd7ad8cf
H A <promisekept836@outlook.com>
2021-06-14 23:59:00 -0400

    refactored using .forEach

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,11 @@ pokemonList.push(
   { name: "Pidgey", height: 0.3, types: ["flying", "normal"] },
   { name: "Nidoking", height: 1.4, types: ["ground", "poison"] }
 );
-for (let i = 0; i < pokemonList.length; i++) {
-  document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height})  `);
-  if (pokemonList[i].height >= 1) {
+
+pokemonList.forEach(function (pokemon) {
+  document.write(`${pokemon.name} (height: ${pokemon.height}) `);
+  if (pokemon.height >= 1) {
     document.write(`Wow, that's big!`);
   }
   document.write(`<br><br>`);
-}
+});
